135a6de792d9193a99165fd0b6230560ef29815c
Carlos Sepulveda <sepul0392@unicauca.edu.co>
2019-10-13 14:34:12 -0500

    Primer Commit

---
+++ src/app/components/inicio-profesores/inicio-profesores.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-profesores',
+  templateUrl: './inicio-profesores.component.html',
+})
+export class InicioProfesoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
