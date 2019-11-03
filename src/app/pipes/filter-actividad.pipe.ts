ad7f881bb7620a9bd428d20702b977434a8824bf
Carlos Sepulveda <sepul0392@unicauca.edu.co>
2019-11-03 12:30:19 -0500

    listado y filtrado de actividades y contenidoREA en WEB Busqueda funcionando

--- src/app/pipes/filter-actividad.pipe.ts
+++ src/app/pipes/filter-actividad.pipe.ts
@@ -5,8 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterActividadPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
-  }
+  //transform(value: any, ...args: any[]): any {
+    transform(items: any, nombreSearch: string, materiaSearch: number, gradoSearch: number, docenteSearch: number) {
+      //console.log("entradas:", nombreSearch,  materiaSearch, gradoSearch, docenteSearch);
+      //console.log("item:", items);
+      if (items && items.length) {
+        return items.filter(item => {
+          if (nombreSearch && item.titulo_actividad.toLowerCase().indexOf(nombreSearch.toLowerCase()) === -1) {
+            return false;
+          }
+          if (materiaSearch && item.id_materia != materiaSearch) {
+            return false;
+          }
+          if (gradoSearch && item.id_grado != gradoSearch) {
+            return false;
+          }
+          if (docenteSearch && item.id_docente != docenteSearch) {
+            return false;
+          }
+          return true;
+        })
+      }
+      else {
+        return items;
+      }
+    }
 
 }
