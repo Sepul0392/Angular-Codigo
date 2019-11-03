9522f3e74437dcc702480785b12375c655606cae
Carlos Sepulveda <sepul0392@unicauca.edu.co>
2019-11-02 21:37:05 -0500

    configuracion filtro de competencias con pequeño mensaje de error al iniciar

--- src/app/models/competencia.ts
+++ src/app/models/competencia.ts
@@ -2,6 +2,6 @@ export interface CompetenciaI {
     id_competencia: number,
     nombre_competencia: string,
     id_areaMateria: number,
-    gradiInicial: number,
+    gradoInicial: number,
     gradoFinal: number
   }
--- src/app/models/materia.ts
+++ src/app/models/materia.ts
@@ -2,6 +2,6 @@ export interface MateriaI {
     id_materia: number,
     nombre_materia: string,
     id_colegio: number,
-    id_materiaCompetencia: number,
+    id_areaMateria: number,
     url_imagen: string
 }
