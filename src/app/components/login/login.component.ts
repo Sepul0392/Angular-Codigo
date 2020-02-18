20d60189cf2aad221f5eae7d2810f17909288fa7
Carlos Sepulveda <sepul0392@unicauca.edu.co>
2020-02-18 12:11:09 -0500

    modificacion ubicacion archivo apk

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -124,7 +124,7 @@ export class LoginComponent implements OnInit {
 
   downloadApp(){
     console.log('Descargando');
-    const urlLoad = 'assets/img/SmartFC-App.apk';
+    const urlLoad = 'assets/SmartFC-App.apk';
     window.open(urlLoad, "_blank");
   }
 
